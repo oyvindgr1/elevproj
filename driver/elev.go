0600b8028aa9dc1850bd9f9752fc6473af4a0b90
oyvindgr1 <oyvigre@stud.ntnu.no>
2015-02-13 14:06:07 +0100

    Forandret elev.go

---
+++ driver/elev.go
@@ -0,0 +1,44 @@
+package driver
+
+import (
+	"fmt"
+	"elevproj/io.go"
+
+
+)
+
+type Order struct {
+	Floor int
+	Direction 
+}
+	
+
+const N_BUTTONS 3
+
+
+func Init() () {
+	if !IoInit() {
+		fmt.Printf("IO initiated\n")
+	}
+	else {
+		fmt.Printf("IO not initiated\n")
+	}
+	//Clear the lights!
+	
+
+}
+
+func setDoorOpenLamp(int i) {
+	if  i == 1 {
+		Set_bit(LIGHT_DOOR_OPEN)
+	}
+	else {
+		Clear_bit(LIGHT_DOOR_OPEN)
+	}
+}
+
+func getObstructionSignal() bool {
+	return Read_bit(OBSTRUCTION)
+}
+
+	
