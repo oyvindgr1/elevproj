af3a53ff48ed27683814161912ca346473a4c646
oyvindgr1 <oyvigre@stud.ntnu.no>
2015-03-06 09:33:38 +0100

    Test drive added

---
+++ driver/test.go
@@ -0,0 +1,44 @@
+
+package main
+import (
+"elev"
+"fmt"
+"time"
+)
+
+func main(){
+	fmt.Printf("Current floor: ", elev.GetFloorSensorSignal())
+	elev.SetLightFloorIndicator(elev.GetFloorSensorSignal)
+	fmt.Printf("Drive!")	
+	elev.SetSpeed(20)
+	time.Sleep(2*time.Second)
+	fmt.Printf("Stop!")
+	elev.SetSpeed(0)
+
+	fmt.Printf("Button lamps on")
+	elev.SetButtonLamp(ORDER_UP, 0, 1) 
+	elev.SetButtonLamp(ORDER_UP, 1, 1) 
+	elev.SetButtonLamp(ORDER_UP, 2, 1) 
+
+	elev.SetButtonLamp(ORDER_DOWN, 1, 1) 
+	elev.SetButtonLamp(ORDER_DOWN, 2, 1) 
+	elev.SetButtonLamp(ORDER_DOWN, 3, 1) 
+
+	fmt.Printf("Button lamps off")
+	elev.SetButtonLamp(ORDER_UP, 0, 0) 
+	elev.SetButtonLamp(ORDER_UP, 1, 0) 
+	elev.SetButtonLamp(ORDER_UP, 2, 0) 
+
+	elev.SetButtonLamp(ORDER_DOWN, 1, 0) 
+	elev.SetButtonLamp(ORDER_DOWN, 2, 0) 
+	elev.SetButtonLamp(ORDER_DOWN, 3, 0) 
+
+
+	fmt.Printf("Open door")
+	elev.SetOpenDoor(1)
+	fmt.Printf("Stop lamp")
+	elev.SetStopLamp(1)
+	fmt.Printf("Door open")
+	elev.SetDoorOpenLamp(1)
+}
+
